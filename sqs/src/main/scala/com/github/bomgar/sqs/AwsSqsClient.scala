9ea995c80accabf20bad6ebaaa738e01ee9dd4c8
Patrick Haun <bomgar85@googlemail.com>
2015-05-02 14:08:12 +0200

    map listqueueresult to domain object

--- sqs/src/main/scala/com/github/bomgar/sqs/AwsSqsClient.scala
+++ sqs/src/main/scala/com/github/bomgar/sqs/AwsSqsClient.scala
@@ -3,9 +3,10 @@ package com.github.bomgar.sqs
 import com.github.bomgar.Region
 import com.github.bomgar.auth.credentials.AwsCredentialsProvider
 import com.github.bomgar.client.BaseAwsClient
+import com.github.bomgar.sqs.domain.Queue
 import play.api.libs.ws.WSClient
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Future, ExecutionContext}
 
 
 class AwsSqsClient(credentialsProvider: AwsCredentialsProvider, region: Region.Type, client: WSClient)(implicit executionContext: ExecutionContext)
@@ -13,7 +14,7 @@ class AwsSqsClient(credentialsProvider: AwsCredentialsProvider, region: Region.T
 
 
 
-  def listQueues() = {
+  def listQueues(): Future[Seq[Queue]] = {
     val response = client.url(baseUrl)
       .withHeaders(
         "Content-Type" -> "application/x-www-form-urlencoded"
@@ -23,7 +24,7 @@ class AwsSqsClient(credentialsProvider: AwsCredentialsProvider, region: Region.T
 
     response.map{
       response =>
-        response.xml
+        (response.xml \\ "QueueUrl").map(queueUrl => new Queue(queueUrl.text)).toSeq
     }
   }
 }
---
+++ sqs/src/main/scala/com/github/bomgar/sqs/domain/Queue.scala
@@ -0,0 +1,3 @@
+package com.github.bomgar.sqs.domain
+
+case class Queue(url: String)
