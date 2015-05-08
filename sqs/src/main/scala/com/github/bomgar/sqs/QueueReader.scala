bf12af7b231a5fb9a32c5f442508772396f60b3e
Patrick Haun <bomgar85@googlemail.com>
2015-05-08 20:40:34 +0200

    some documentation

--- sqs/src/main/scala/com/github/bomgar/sqs/QueueReader.scala
+++ sqs/src/main/scala/com/github/bomgar/sqs/QueueReader.scala
@@ -4,6 +4,21 @@ import com.github.bomgar.sqs.domain.{Message, QueueReference}
 
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ * maxNumberOfMessages
+ *
+ * The maximum number of messages to return. Amazon SQS never returns
+ * more messages than this value but may return fewer. Values can be from
+ * 1 to 10. Default is 1. All of the messages are not necessarily
+ * returned.
+ *
+ * waitTimeInSeconds
+ *
+ * The duration (in seconds) for which the call will wait for a message
+ * to arrive in the queue before returning. If a message is available,
+ * the call will return sooner than WaitTimeSeconds.
+ *
+ */
 class QueueReader private[sqs](sqsClient: AwsSqsClient, queueReference: QueueReference)(implicit executionContext: ExecutionContext) {
 
   def receiveSingleMessage: Future[Option[Message]] =
