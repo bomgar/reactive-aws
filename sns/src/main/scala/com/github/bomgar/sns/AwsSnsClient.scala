e451ddd7c953babe4fbc77a9cc2edfb78408273a
mathissimo <mmsec@t101.de>
2015-05-22 12:07:45 +0200

    Renamed evn-vars for aws-credentials in SNS module

--- sns/src/main/scala/com/github/bomgar/sns/AwsSnsClient.scala
+++ sns/src/main/scala/com/github/bomgar/sns/AwsSnsClient.scala
@@ -4,7 +4,7 @@ import com.github.bomgar.Region
 import com.github.bomgar.auth.credentials.AwsCredentialsProvider
 import com.github.bomgar.client.BaseAwsClient
 import com.github.bomgar.sns.domain.TopicReference
-  import play.api.libs.ws.WSClient
+import play.api.libs.ws.WSClient
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
