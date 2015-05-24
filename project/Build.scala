0091e8dcf5abc61277e98543f6a4fc53131d1a75
Patrick Haun <bomgar85@googlemail.com>
2015-05-24 12:43:49 +0200

    only use one build file

--- project/Build.scala
+++ project/Build.scala
@@ -3,20 +3,56 @@ import sbt._
 
 object Build extends Build {
 
-  lazy val core = project in file("core")
+  val specs2Version = "3.6"
+  val playVersion = "2.3.9"
 
-  lazy val sqs = project.in(file("sqs")).dependsOn(core)
+  val testDependencies = Seq(
+    "org.specs2" %% "specs2-core" % specs2Version,
+    "org.specs2" %% "specs2-mock" % specs2Version,
+    "com.typesafe.play" %% "play-test" % playVersion
+  ).map(_ % "test")
 
-  lazy val sns = project.in(file("sns")).dependsOn(core)
+  val libDependencies = Seq(
+    "com.typesafe.play" %% "play-ws" % playVersion
+  )
+
+
+  val commonSettings = Seq(
+    version := "1.0",
+    organization := "com.github.bomgar",
+    scalaVersion := "2.11.6",
+    scalacOptions := Seq("-target:jvm-1.8"),
+    testOptions in Test := Seq(Tests.Argument("exclude", "integration")),
+    resolvers ++= Seq(
+      "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+      "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+    ),
+    libraryDependencies := libDependencies ++ testDependencies,
+    ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) }
+  )
+
+  lazy val core = (project in file("core"))
+    .settings(commonSettings)
+    .settings(name := "reactive-aws-core")
+
+  lazy val sqs = project.in(file("sqs"))
+    .dependsOn(core)
+    .settings(commonSettings)
+    .settings(name := "reactive-aws-sqs")
+
+  lazy val sns = project.in(file("sns"))
+    .dependsOn(core)
+    .settings(commonSettings)
+    .settings(name := "reactive-aws-sns")
 
   lazy val root =
     Project(
       id = "reactive-aws",
-      base = file(".")
-    ).settings(
-        name := "reactive-aws",
-        organization := "com.github.bomgar",
-        version := "1.0",
-        scalaVersion := "2.11.6"
-      ).aggregate(sqs, sns, core)
+      base = file("."),
+      settings = commonSettings
+    )
+      .settings(
+        name := "reactive-aws"
+      )
+      .aggregate(sqs, sns, core)
 }
